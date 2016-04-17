87c0b5c0f116ecf51d7c87986ba9742671a19ca2
idarv <twintip_idar@hotmail.com>
2016-04-18 01:15:54 +0200

    shorten python (164)

--- python/equalwords_short.py
+++ python/equalwords_short.py
@@ -1,4 +1,3 @@
 a=dict()
-k='\n'
-for l in open('words.txt', encoding='utf-8'):a.setdefault(''.join(sorted(l.strip(k))),[]).append(l.strip(k))
-for v in sorted(a.values()):print(', '.join(map(str, v)))
+for l in open('words.txt', encoding='utf-8'):a.setdefault(''.join(sorted(l)),[]).append(l.strip())
+for v in sorted(a.values()):print(', '.join(map(str,v)))
