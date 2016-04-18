9aeaa1abfa57c7b90f4970c54feceb9902276015
Jonas Jensen <jonas.jensen@msn.com>
2016-04-18 11:00:37 +0200

    replace one-line semicolon with linebreak (171)

--- javascript/equalwords_short.js
+++ javascript/equalwords_short.js
@@ -1,3 +1,4 @@
 d={}
 require('fs').readFileSync('words.txt','utf8').split('\r\n').map(i=>{a=i.split('').sort().join()
-d[a]?d[a].push(i):d[a]=[i]});for(i in d)console.log(d[i].join(', '))
+d[a]?d[a].push(i):d[a]=[i]})
+for(i in d)console.log(d[i].join(', '))
