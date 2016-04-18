3c123470487165b9b49bbf4609c65e1d4f18da50
Jonas Jensen <jonas.jensen@msn.com>
2016-04-18 10:59:17 +0200

    shorten js (171)

--- javascript/equalwords_short.js
+++ javascript/equalwords_short.js
@@ -1,3 +1,3 @@
-var d={}
-require('fs').readFileSync('words.txt','utf8').split('\n').map(i=>{i=i.trim();var a=i.split('').sort().join('');d[a]?d[a].push(i):d[a]=[i]})
-for(i in d){console.log(d[i].join(', '))}
+d={}
+require('fs').readFileSync('words.txt','utf8').split('\r\n').map(i=>{a=i.split('').sort().join()
+d[a]?d[a].push(i):d[a]=[i]});for(i in d)console.log(d[i].join(', '))
