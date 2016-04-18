95b89330113805e71826d7cdaae6bad38f23ee9e
idarv <twintip_idar@hotmail.com>
2016-04-18 20:43:32 +0200

    slice string instead of strip (130)

--- python/equalwords_short.py
+++ python/equalwords_short.py
@@ -1,3 +1,3 @@
 a=dict()
-for l in open('words.txt'):a.setdefault(str(sorted(l)),[]).append(l.strip())
+for l in open('words.txt'):a.setdefault(str(sorted(l)),[]).append(l[:-1])
 for v in sorted(a.values()):print(', '.join(v))
