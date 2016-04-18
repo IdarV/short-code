86dde3c36ccc5b2d681c267204370031c166653e
IdarV <tjoida13@nith.no>
2016-04-18 16:56:58 +0200

    remove encoding (133)

--- python/equalwords_short.py
+++ python/equalwords_short.py
@@ -1,3 +1,3 @@
 a=dict()
-for l in open('words.txt',encoding='utf8'):a.setdefault(str(sorted(l)),[]).append(l.strip())
+for l in open('words.txt'):a.setdefault(str(sorted(l)),[]).append(l.strip())
 for v in sorted(a.values()):print(', '.join(v))
