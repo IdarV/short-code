c1334763065759546249e87d6e5a7bd65accf9dc
IdarV <twintip_idar@hotmail.com>
2016-04-22 14:44:52 +0200

    truncated perl

--- perl/equalwords_short.pl
+++ perl/equalwords_short.pl
@@ -1,6 +1,3 @@
 my %h;open F,'words.txt';
-while($r=<F>){
-  chop $r;$s=join('',sort(split('',$r)));
-  if(!exists $h{$s}){$h{$s}=[]}push(@{%h{$s}},$r)
-}
+while($r=<F>){chop $r;$s=join('',sort(split('',$r)));if(!exists $h{$s}){$h{$s}=[]}push(@{%h{$s}},$r)}
 foreach $a(values %h){print join ', ',@{$a};print "\n";}
