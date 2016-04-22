b8d974723b7e842c5e0b85a63d81dbfb2e703fa7
IdarV <twintip_idar@hotmail.com>
2016-04-22 14:11:53 +0200

    shorten perl (193)

--- perl/equalwords_short.pl
+++ perl/equalwords_short.pl
@@ -1,3 +1,3 @@
-my %h=();open(my $f, 'words.txt');
-while (my $r = <$f>) {chomp $r;$s=join('', sort(split('', $r)));if(!exists $h{$s}){$h{$s}=[]}push(@{%h{$s}}, $r)}
+my %h;open $f,'words.txt';
+while(my $r=<$f>){chomp $r;$s=join('',sort(split('',$r)));if(!exists $h{$s}){$h{$s}=[]}push(@{%h{$s}},$r)}
 foreach my $a(values %h){print join ', ',@{$a};print "\n";}
