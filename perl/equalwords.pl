c9ad9baf97946205d8f2deae093e97cc857ef7fb
IdarV <twintip_idar@hotmail.com>
2016-04-22 13:38:02 +0200

    perl works

--- perl/equalwords.pl
+++ perl/equalwords.pl
@@ -1,36 +1,19 @@
-# my $filename = 'words.txt';
-# open(my $fh, '<:encoding(UTF-8)', $filename)
-#   or die "Could not open file '$filename' $!";
-#
-# while (my $row = <$fh>) {
-#   chomp $row;
-#   print "$row\n";
-# }
+my %hash = ();
+my $filename = 'words.txt';
+open(my $fh, $filename);
 
-my %dramatis_personae = (
-    humans => [ 'hamnet', 'shakespeare', 'robyn', ],
-    faeries => [ 'oberon', 'titania', 'puck', ],
-    other => [ 'morpheus, lord of dreams' ],
-);
-my $name='Jonas';
-my $grp='new';
-unless(exists $dramatis_personae{$grp}){
-  $dramatis_personae{$grp} = [];
+while (my $row = <$fh>) {
+  chomp $row;
+  $abc_word =  join('', sort(split('', $row)));
+  unless(exists $hash{$abc_word}){
+    $hash{$abc_word} = [];
+  }
+  push(@{%hash{$abc_word}}, $row);
 }
 
-push(@{%dramatis_personae{$grp}}, $name);
-# push(@{%dramatis_personae{$grp}}, $name);
-
-foreach my $group (keys %dramatis_personae) {
-    print "The members of $group are\n";
-    foreach (@{$dramatis_personae{$group}}) {
-        print "\t$_\n";
+foreach my $group (keys %hash) {
+    foreach (@{$hash{$group}}) {
+        print "$_,";
     }
+        print "\n";
 }
-
-
-
-
-
-# my %hash;
-# @hash{@arr} = 0..$#arr;
