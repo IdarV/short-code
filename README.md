2451b8fe5dae3575668df98bfd62f1a076eb8aeb
IdarV <twintip_idar@hotmail.com>
2016-04-24 10:40:41 +0200

    shortened perl (116)

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Rules:
 Current results:
 
 1. Ruby: 101
-2. Perl: 118
+2. Perl: 116
 3. Python: 130
 4. Javascript: 143
 
--- perl/equalwords_short.pl
+++ perl/equalwords_short.pl
@@ -1,3 +1,3 @@
 %h;open F,'words.txt';
-for(<F>){chop $_;$h{join '',sort split '',$_}.="$_, "}
+for(<F>){chop $_;$h{join//,sort split//,$_}.="$_, "}
 map{print substr($_,0,-2)."\n"}values %h
