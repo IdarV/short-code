a8c3d3a2bdeb03fb565fabcc6c29443d85a3652c
Tomas Ruud <tomasruu@gmail.com>
2016-04-25 13:08:16 +0200

    Less characters

--- elixir/equalwords_short.exs
+++ elixir/equalwords_short.exs
@@ -1,4 +1,1 @@
-File.read!("words.txt")|>String.split("\n")|>Enum.reduce(%{},fn(a, b)->c=String.split(a,"")|>Enum.sort|>List.to_string
-d=Map.get(b,c,"")
-Map.put(b,c,"#{d}, #{a}")end)|>Map.values|>Enum.each(fn(a)->String.split_at(a,2)|>elem(1)|>IO.puts
-end)
+import Enum;import Map;File.read!("words.txt")|>String.split("\n")|>reduce(%{},fn(a, b)->c=String.split(a,"")|>sort|>List.to_string;d=get(b,c,"");put(b,c,"#{d}, #{a}")end)|>values|>each(fn(a)->String.split_at(a,2)|>elem(1)|>IO.puts;end)
