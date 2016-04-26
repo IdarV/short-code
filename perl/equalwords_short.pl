c279f9295d4fb42d74ed3c57f9c6c10efea6acd1
IdarV <twintip_idar@hotmail.com>
2016-04-26 14:59:10 +0200

    shortened perl

--- perl/equalwords_short.pl
+++ perl/equalwords_short.pl
@@ -1,2 +1,3 @@
-
-%h;open F,'words.txt';for(<F>){chop$_;$h{join//,sort split//,$_}.=", $_"}map{print substr$_,2,.$/}values%h
+# %h;open F,'words.txt';for(<F>){chop$_;$h{join//,sort split//,$_}.=", $_"}map{print substr$_,2,.$/}values%h
+# %h;open F,'words.txt';for(<F>){chop;$h{join//,sort$_=~/./g}.=", $_"}map{print substr$_,2,.$/}values%h
+%h;open F,'words.txt';for(<F>){chop;$h{join//,sort$_=~/./g}.=", $_"}map{print substr$_,2,.$/}values%h
