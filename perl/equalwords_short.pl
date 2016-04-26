0cca32d83a445eda26b0550a2a93035756862164
IdarV <twintip_idar@hotmail.com>
2016-04-26 08:18:58 +0200

    mrtn shortened perl

--- perl/equalwords_short.pl
+++ perl/equalwords_short.pl
@@ -1,3 +1,2 @@
-%h;open F,'words.txt';
-for(<F>){chop$_;$h{join//,sort split//,$_}.=", $_"}
-map{print substr$_,2,."\n"}values%h
+
+%h;open F,'words.txt';for(<F>){chop$_;$h{join//,sort split//,$_}.=", $_"}map{print substr$_,2,.$/}values%h
