0391a181a38e3243c423d8b1798a748335ce188a
Idar Vassdal <idarv@mesan.no>
2016-10-23 00:17:36 +0200

    added working go example

--- go/equalwords.go
+++ go/equalwords.go
@@ -4,23 +4,26 @@ import(
     "fmt"
     "io/ioutil"
     "strings"
-
+    "sort"
 )
 
 func main() {
-     //Open the file.
-     // f, _ := os.Open("../words.txt")
-     //Create a new Scanner for the file.
-     //scanner := bufio.NewScanner(f)
-     //Loop over all lines in the file and print them.
-     //for scanner.Scan() {
-	 //line := scanner.Text()
-	 //fmt.Println(line)
-     //}
-	content, _  := ioutil.ReadFile("../words.txt")
-	 fmt.Println(content)
-    lines := strings.Split(string(content), "\r\n")
-     for _,line := range lines{
-        fmt.Println(line)
+    content, _  := ioutil.ReadFile("words.txt")
+    var anagrams = make(map[string][]string)
+    lines := strings.Split(string(content), "\n")
+
+    for _,line := range lines{
+        if(line == ""){
+            break;
+        }
+
+        var sortedLine = strings.Split(line, "")
+        sort.Strings(sortedLine);
+        var sortedLineAsString = strings.Join(sortedLine, "")
+        anagrams[sortedLineAsString] = append(anagrams[sortedLineAsString], line)
+    }
+
+    for _, v := range anagrams{
+        fmt.Println(strings.Join(v, ", "))
     }
 }
