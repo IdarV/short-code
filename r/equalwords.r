07a71c698d6e28966e250e90b518c5caebf2021f
Idar Vassdal <idarv@mesan.no>
2017-02-18 00:22:31 +0100

    first working example

--- r/equalwords.r
+++ r/equalwords.r
@@ -1,1 +1,13 @@
-print ("Hello, World!")
+fileName="../words.txt"
+res <- readLines(fileName)
+h <- new.env(hash=T, parent=emptyenv())
+
+for (r in res){
+    sortedWord = paste(sort(unlist(strsplit(r, ""))), collapse = "")
+    h[[sortedWord]] = paste(h[[sortedWord]],r)
+}
+
+for (v in ls(h)) {
+    cat(paste(h[[v]], '\n'))
+}
+
