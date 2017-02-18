d127974ca3af27285fa63bd5f32ad5989eaf5c9f
Idar Vassdal <idarv@mesan.no>
2017-02-18 01:30:30 +0100

    shortened R (150)

--- r/equalwords_short.r
+++ r/equalwords_short.r
@@ -1,1 +1,1 @@
-f=readLines('../words.txt');h=new.env();for(r in f){s=toString(sort(unlist(strsplit(r,''))));h[[s]]=paste(h[[s]],r)};for(v in ls(h))cat(paste(h[[v]],'\n'))
+h=new.env();for(r in readLines('../words.txt'){s=toString(sort(unlist(strsplit(r,''))));h[[s]]=paste(h[[s]],r)};for(v in ls(h))cat(paste(h[[v]],'\n'))
