82ddd5b37c964e3d5dc16544b2116909711faa92
Daniel Rødskog <danielr@mesan.no>
2017-02-25 13:48:49 +0100

    Cleanup and print word list

--- haskell/equalwords.hs
+++ haskell/equalwords.hs
@@ -1,18 +1,4 @@
-import System.IO
-import Data.List
-import Data.Map.Lazy
-import Prelude hiding (null, lookup, map, filter)
-
--- LOL HASKELL IDFK WHAT IM DOING WAT IS THIS
-
-myArray = [("kygo", "On, Off"), ("no", "No, Mans"), ("hihi", "ION, DEAN")]
--- hashMap = fromList [("kygo" :: String, "On, Off"), ("no", "No, Mans"), ("hihi", "ION, DEAN")]
-
 main = do
-    -- [("new", "haha")] : myArray;
-    -- let hashMap = fromList myArray;
-    -- print $ myArray
-    -- print $ keys hashMap
-    -- print $ elems hashMap
-    words <- readFile "../words.txt"
-    putStr words
+    input <- readFile "../words.txt"
+    let words = lines input
+    print $ words
