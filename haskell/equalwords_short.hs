d898788b6ae17968ae255a00036a62481e3306f0
Daniel Rødskog <danielr@mesan.no>
2017-02-25 14:31:20 +0100

    Haskell 123

--- haskell/equalwords_short.hs
+++ haskell/equalwords_short.hs
@@ -1,3 +1,4 @@
 import Data.List
 import GHC.Exts
-main=readFile"../words.txt">>=putStrLn.intercalate"\n".map(intercalate", ").groupWith sort.lines
+j=intercalate
+main=readFile"../words.txt">>=putStrLn.j"\n".map(j", ").groupWith sort.lines
