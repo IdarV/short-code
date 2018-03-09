625a1cfd9ad6b0c98e78a5fbe37959483ebf271a
Jonas Jensen <jonas.jensen@greenbird.com>
2018-03-09 22:53:52 +0100

    fix equalwords words.txt filepath

--- javascript/equalwords.js
+++ javascript/equalwords.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var dict = {};
-var array = fs.readFileSync('words.txt').toString().split("\n");
+var array = fs.readFileSync('../words.txt').toString().split("\n");
 for(i in array) {
     var abc_word = array[i].split('').sort().join('');
     if(abc_word in dict){
